04211f77f5a8ad41ca7317188f06c19ee3554851
Fried Hoeben <github@hsac.nl>
2017-09-26 21:34:14 +0200

    Add some initial (empty) Java classes

---
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AppiumDriverSetup.java
@@ -0,0 +1,9 @@
+package nl.hsac.fitnesse.fixture.slim.mobile;
+
+import nl.hsac.fitnesse.fixture.slim.web.SeleniumDriverSetup;
+
+/**
+ * Fixture to connect FitNesse to appium.
+ */
+public class AppiumDriverSetup extends SeleniumDriverSetup {
+}
---
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/MobileTest.java
@@ -0,0 +1,10 @@
+package nl.hsac.fitnesse.fixture.slim.mobile;
+
+import io.appium.java_client.MobileElement;
+import nl.hsac.fitnesse.fixture.slim.web.BrowserTest;
+
+/**
+ * Fixture to test using Appium.
+ */
+public class MobileTest<T extends MobileElement> extends BrowserTest<T> {
+}
