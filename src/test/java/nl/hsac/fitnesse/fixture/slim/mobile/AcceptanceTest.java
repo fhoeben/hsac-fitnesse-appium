5f777cee75adc05e1b772a60e6632748557fcf8c
Fried Hoeben <github@hsac.nl>
2017-10-01 22:53:36 +0200

    Prepare for first android test

---
+++ src/test/java/nl/hsac/fitnesse/fixture/slim/mobile/AcceptanceTest.java
@@ -0,0 +1,13 @@
+package nl.hsac.fitnesse.fixture.slim.mobile;
+
+import fitnesse.junit.FitNesseRunner;
+import nl.hsac.fitnesse.junit.HsacFitNesseRunner;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class to run wiki suite with this project's tests.
+ */
+@RunWith(HsacFitNesseRunner.class)
+@FitNesseRunner.Suite("AppiumTests")
+public class AcceptanceTest {
+}
