9e8ad6015416c2a5cfa49ede25132a674ea083b9
Fried Hoeben <github@hsac.nl>
2017-10-03 20:08:46 +0200

    Use hiding of keys as provided by selenium driver setup

--- src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AppiumDriverSetup.java
+++ src/main/java/nl/hsac/fitnesse/fixture/slim/mobile/AppiumDriverSetup.java
@@ -12,9 +12,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
@@ -23,7 +20,6 @@ import java.util.function.BiFunction;
  */
 public class AppiumDriverSetup extends SeleniumDriverSetup {
     private static final String APP_CAPABILITY_NAME = "app";
-    private final List<String> secretCapabilities = new ArrayList<>();
 
     static {
         // ensure our helpers are used for Appium WebDrivers
@@ -58,40 +54,5 @@ public class AppiumDriverSetup extends SeleniumDriverSetup {
             desiredCapabilities.setCapability(APP_CAPABILITY_NAME, fullPath);
         }
         return super.createAndSetRemoteWebDriver(constr, url, desiredCapabilities);
-    }
-
-    @Override
-    protected String describeCapabilities(RemoteWebDriver remoteDriver) {
-        Capabilities capabilities = remoteDriver.getCapabilities();
-        Map<String, ?> capaToShow = getCapabilitiesToDescribe(capabilities);
-        StringBuilder result = new StringBuilder("<table><tbody>");
-        for (Map.Entry<String, ?> entry : capaToShow.entrySet()) {
-            result.append("<tr><th>");
-            result.append(entry.getKey());
-            result.append("</th><td>");
-            result.append(entry.getValue());
-            result.append("</td></tr>");
-        }
-        result.append("</tbody></table>");
-        return result.toString();
-    }
-
-    protected Map<String, ?> getCapabilitiesToDescribe(Capabilities capabilities) {
-        List<String> secrets = getSecretCapabilities();
-
-        Map<String, Object> capaToShow = new LinkedHashMap<>();
-        for (Map.Entry<String, ?> entry : capabilities.asMap().entrySet()) {
-            String key = entry.getKey();
-            if (secrets.contains(key)) {
-                capaToShow.put(key, "*****");
-            } else {
-                capaToShow.put(key, entry.getValue());
-            }
-        }
-        return capaToShow;
-    }
-
-    public List<String> getSecretCapabilities() {
-        return secretCapabilities;
     }
 }
