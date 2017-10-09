e0878ba3d50f080a96fba65ef3d3e2f2683eec9c
Fried Hoeben <github@hsac.nl>
2017-10-09 13:43:27 +0200

    Use class chains instead of XPath to find elements for iOS

--- src/main/java/nl/hsac/fitnesse/fixture/util/mobile/by/IOSBy.java
+++ src/main/java/nl/hsac/fitnesse/fixture/util/mobile/by/IOSBy.java
@@ -1,35 +1,30 @@
 package nl.hsac.fitnesse.fixture.util.mobile.by;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.ios.IOSElement;
 import nl.hsac.fitnesse.fixture.util.selenium.by.HeuristicBy;
-import nl.hsac.fitnesse.fixture.util.selenium.by.XPathBy;
+import nl.hsac.fitnesse.fixture.util.selenium.by.LazyPatternBy;
 import org.openqa.selenium.By;
 
 public class IOSBy {
-    private static final String CONTAINS_EXACT = "[@name='%1$s' " +
-            "or @hint='%1$s' " +
-            "or @label='%1$s' " +
-            "or @value='%1$s']";
+    private static final String CONTAINS_EXACT = "[`name==\"%1$s\" OR label==\"%1$s\"` OR value==\"%1$s\"` OR hint==\"%1$s\"`]";
 
-    private static final String CONTAINS_PARTIAL = "[contains(@name,'%1$s') " +
-            "or contains(@hint,'%1$s') " +
-            "or contains(@label,'%1$s') " +
-            "or contains(@value,'%1$s')]";
+    private static final String CONTAINS_PARTIAL = "[`name CONTAINS \"%1$s\" OR label CONTAINS \"%1$s\"` OR value CONTAINS \"%1$s\"` OR hint CONTAINS \"%1$s\"`]";
 
     public static By exactText(String text) {
-        return new XPathBy(".//*" + CONTAINS_EXACT, text);
+        return new ClassChain("**/*" + CONTAINS_EXACT, text);
     }
 
     public static By partialText(String text) {
-        return new XPathBy(".//*" + CONTAINS_PARTIAL, text);
+        return new ClassChain("**/*" + CONTAINS_PARTIAL, text);
     }
 
     public static By exactButtonText(String text) {
-        return new XPathBy(".//XCUIElementTypeButton" + CONTAINS_EXACT, text);
+        return new ClassChain("**/XCUIElementTypeButton" + CONTAINS_EXACT, text);
     }
 
     public static By partialButtonText(String text) {
-        return new XPathBy(".//XCUIElementTypeButton" + CONTAINS_PARTIAL, text);
+        return new ClassChain("**/XCUIElementTypeButton" + CONTAINS_PARTIAL, text);
     }
 
     public static HeuristicBy<IOSElement> heuristic(String text) {
@@ -38,5 +33,23 @@ public class IOSBy {
 
     public static HeuristicBy<IOSElement> buttonHeuristic(String text) {
         return new HeuristicBy<>(exactButtonText(text), partialButtonText(text));
+    }
+
+    public static class ClassChain extends LazyPatternBy {
+        /**
+         * Creates By based on pattern, supporting placeholder replacement.
+         * Pattern will only be filled in when By is evaluated.
+         *
+         * @param pattern    basic pattern, possibly with placeholders {@link String#format}.
+         * @param parameters values for placeholders.
+         */
+        public ClassChain(String pattern, String... parameters) {
+            super(pattern, parameters);
+        }
+
+        @Override
+        protected By createNested(String expr) {
+            return MobileBy.iOSClassChain(expr);
+        }
     }
 }
